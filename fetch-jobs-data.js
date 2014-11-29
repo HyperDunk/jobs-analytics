a1cf3c5b39d58087f1b01e29493933af709c5a1d
Aakarsh <aakarsh.mhm@gmail.com>
2014-11-28 23:31:08 -0800

    Modified data format for query 2a and added a new page for it

--- fetch-jobs-data.js
+++ fetch-jobs-data.js
@@ -1,6 +1,6 @@
 function query1(company, jobtype, title, startDate, endDate) {
 	
-	url = "http://localhost:8983/solr/collection1/select?q=company%3A%22Gestion+Estrategica%22%0Ajobtype%3A%22Tiempo+Completo%22%0Atitle%3A%22Industria%22&fq=postedDate%3A%5B2012-11-01T00%3A00%3A00Z+TO+2012-11-30T00%3A00%3A00Z%5D&rows=0&wt=json&indent=true";
+	/*url = "http://localhost:8983/solr/collection1/select?q=company%3A%22Gestion+Estrategica%22%0Ajobtype%3A%22Tiempo+Completo%22%0Atitle%3A%22Industria%22&fq=postedDate%3A%5B2012-11-01T00%3A00%3A00Z+TO+2012-11-30T00%3A00%3A00Z%5D&rows=0&wt=json&indent=true";
 	
 	$.ajax({
        type:'GET',
@@ -11,7 +11,13 @@ function query1(company, jobtype, title, startDate, endDate) {
        error: function(data){
            alert('error: ' + data);        
        }
-    });
+    });*/
+
+	var arr = new Object();
+	arr["abc"] = 1;
+	arr["xyz"] = 10;
+	var jsonstring = JSON.stringify(arr);
+	alert(jsonstring);
 	
 }
 
@@ -21,30 +27,30 @@ var timedCountriesCount = [];
 function query2() {
 
 	var companiesList = ["Gestion Estrategica", "GB Consultora"];
-	var dates = [["2012-01-01T00:00:00Z","2012-01-31T00:00:00Z"],
-				["2012-02-01T00:00:00Z","2012-02-28T00:00:00Z"],
-				["2012-03-01T00:00:00Z","2012-03-31T00:00:00Z"],
-				["2012-04-01T00:00:00Z","2012-04-30T00:00:00Z"],
-				["2012-05-01T00:00:00Z","2012-05-31T00:00:00Z"],
-				["2012-06-01T00:00:00Z","2012-06-30T00:00:00Z"],
-				["2012-07-01T00:00:00Z","2012-07-31T00:00:00Z"],
-				["2012-08-01T00:00:00Z","2012-08-31T00:00:00Z"],
-				["2012-09-01T00:00:00Z","2012-09-30T00:00:00Z"],
-				["2012-10-01T00:00:00Z","2012-10-31T00:00:00Z"],
-				["2012-11-01T00:00:00Z","2012-11-30T00:00:00Z"],
-				["2012-12-01T00:00:00Z","2012-12-31T00:00:00Z"],
-				["2013-01-01T00:00:00Z","2013-01-31T00:00:00Z"],
-				["2013-02-01T00:00:00Z","2013-02-28T00:00:00Z"],
-				["2013-03-01T00:00:00Z","2013-03-31T00:00:00Z"],
-				["2013-04-01T00:00:00Z","2013-04-30T00:00:00Z"],
-				["2013-05-01T00:00:00Z","2013-05-31T00:00:00Z"],
-				["2013-06-01T00:00:00Z","2013-06-30T00:00:00Z"],
-				["2013-07-01T00:00:00Z","2013-07-31T00:00:00Z"],
-				["2013-08-01T00:00:00Z","2013-08-31T00:00:00Z"],
-				["2013-09-01T00:00:00Z","2013-09-30T00:00:00Z"],
-				["2013-10-01T00:00:00Z","2013-10-31T00:00:00Z"],
-				["2013-11-01T00:00:00Z","2013-11-30T00:00:00Z"],
-				["2013-12-01T00:00:00Z","2013-12-31T00:00:00Z"]];
+	var dates = [["2012-01-01T00:00:00Z","2012-01-31T00:00:00Z", "Jan12"],
+				["2012-02-01T00:00:00Z","2012-02-28T00:00:00Z", "Feb12"],
+				["2012-03-01T00:00:00Z","2012-03-31T00:00:00Z", "Mar12"],
+				["2012-04-01T00:00:00Z","2012-04-30T00:00:00Z", "Apr12"],
+				["2012-05-01T00:00:00Z","2012-05-31T00:00:00Z", "May12"],
+				["2012-06-01T00:00:00Z","2012-06-30T00:00:00Z", "Jun12"],
+				["2012-07-01T00:00:00Z","2012-07-31T00:00:00Z", "Jul12"],
+				["2012-08-01T00:00:00Z","2012-08-31T00:00:00Z", "Aug12"],
+				["2012-09-01T00:00:00Z","2012-09-30T00:00:00Z", "Sep12"],
+				["2012-10-01T00:00:00Z","2012-10-31T00:00:00Z", "Oct12"],
+				["2012-11-01T00:00:00Z","2012-11-30T00:00:00Z", "Nov12"],
+				["2012-12-01T00:00:00Z","2012-12-31T00:00:00Z", "Dec12"],
+				["2013-01-01T00:00:00Z","2013-01-31T00:00:00Z", "Jan13"],
+				["2013-02-01T00:00:00Z","2013-02-28T00:00:00Z", "Feb13"],
+				["2013-03-01T00:00:00Z","2013-03-31T00:00:00Z", "Mar13"],
+				["2013-04-01T00:00:00Z","2013-04-30T00:00:00Z", "Apr13"],
+				["2013-05-01T00:00:00Z","2013-05-31T00:00:00Z", "May13"],
+				["2013-06-01T00:00:00Z","2013-06-30T00:00:00Z", "Jun13"],
+				["2013-07-01T00:00:00Z","2013-07-31T00:00:00Z", "Jul13"],
+				["2013-08-01T00:00:00Z","2013-08-31T00:00:00Z", "Aug13"],
+				["2013-09-01T00:00:00Z","2013-09-30T00:00:00Z", "Sep13"],
+				["2013-10-01T00:00:00Z","2013-10-31T00:00:00Z", "Oct13"],
+				["2013-11-01T00:00:00Z","2013-11-30T00:00:00Z", "Nov13"],
+				["2013-12-01T00:00:00Z","2013-12-31T00:00:00Z", "Dec13"]];
 	i = dates.length-1;
 	getCountriesCountByTime(companiesList, dates, dates[i][0], dates[i][1]);
 
@@ -52,8 +58,10 @@ function query2() {
 
 
 function getCountriesCountByTime(companiesList, dates, startDate, endDate) {
-	var companyCountriesCount = [];
+	//var companyCountriesCount = [];
 	var count = companiesList.length;
+	var columnObject = new Object();
+	columnObject["state"] = dates[i][2];
 
 	for(var companyKey in companiesList) {
 		var url = "http://localhost:8983/solr/collection1/select?q=company%3A%22"+encodeURIComponent(companiesList[companyKey])+"%22&fq=postedDate%3A["+encodeURIComponent(startDate)+"+TO+"+encodeURIComponent(endDate)+"]&wt=json&indent=true&group=true&group.field=country&group.limit=0";
@@ -63,17 +71,19 @@ function getCountriesCountByTime(companiesList, dates, startDate, endDate) {
 			var dateIndex = $.parseJSON(data).responseHeader.params.fq;
 			//alert(company + " : " + countryCount);
 			count--;
-			companyCountriesCount.push([company,countryCount]);
+			//companyCountriesCount.push([company,countryCount]);
+			columnObject[company] = countryCount;
 			//console.log("companyCountriesCount : " + companyCountriesCount);
 			if(count == 0) {
-				timedCountriesCount.push(companyCountriesCount);
+				//timedCountriesCount.push(companyCountriesCount);
+				timedCountriesCount.push(columnObject);
 				console.log("timedCountriesCount : " + timedCountriesCount);
 				i--;
 				if(i >= 0) {
 					getCountriesCountByTime(companiesList, dates, dates[i][0], dates[i][1]);
 				} else {
 					//Call D3 function to populate/build a stacked column graph by using timedCountriesCount
-
+					processData(timedCountriesCount);
 				}
 				
 			}
@@ -98,7 +108,7 @@ function query4() {
 	var seasons = [["2012-01-01T00:00:00Z","2012-02-28T00:00:00Z"],	
 				["2012-03-01T00:00:00Z","2012-05-31T00:00:00Z"],
 				["2012-06-01T00:00:00Z","2012-08-31T00:00:00Z"],
-				["2012-09-01T00:00:00Z""2012-11-30T00:00:00Z"],
+				["2012-09-01T00:00:00Z","2012-11-30T00:00:00Z"],
 				["2012-12-01T00:00:00Z","2013-02-28T00:00:00Z"],
 				["2013-03-01T00:00:00Z","2013-05-31T00:00:00Z"],
 				["2013-06-01T00:00:00Z","2013-08-31T00:00:00Z"],
